687dd33bfd87df1dee987d5a9203d14ee0c1cf7f
DuckToFo <golden4gnvx@gmail.com>
2023-03-09 17:02:53 +0700

    final

--- src/components/QuoteFetching.js
+++ src/components/QuoteFetching.js
@@ -26,28 +26,34 @@ const QuoteFetching = () => {
         setAuthor(!author)
     }
     let display = <div className="container">
-        <button type="button" onClick={randomId} className="button-random">random<ArrowClockwise /></button>
-        <div className="quote-container">
-            "{quotes.results[id].content}"
-        </div>
-        <button type="button" onClick={handleAuthorClick} className="display-button">
-            <div className="author-container">
-                {quotes.results[id].author}
-                <div className="tags">
-                    {quotes.results[id].tags.map(item => <span style={{marginRight: 8}}>{item}</span>)}
-                </div>
-            </div>
-        </button>
-</div>
-    if (author === false) {
-        display = <button type="button" onClick={handleAuthorClick} className="display-button">
+    <button type="button" onClick={randomId} className="button-random">random<ArrowClockwise /></button>
+    <div className="quote-container">
+        "{quotes.results[id].content}"
+    </div>
+    <button type="button" onClick={handleAuthorClick} className="display-button">
         <div className="author-container">
-            {quotes.results[id].author}
+            <div className="author">
+                {quotes.results[id].author}
+            </div>
             <div className="tags">
                 {quotes.results[id].tags.map(item => <span style={{marginRight: 8}}>{item}</span>)}
             </div>
         </div>
     </button>
+</div>
+    if (author === false) {
+        display = <div>
+            <button type="button" onClick={handleAuthorClick} className="display-button">
+        <div className="author-container">
+            <div className="author" style={{fontSize: 24, width: 240}}>
+                {quotes.results[id].author}
+            </div>
+        </div>
+            </button>
+        <div className="quote-container">
+            "{quotes.results[id].content}"
+        </div>
+        </div>
     }
     return (
         <div className="container">
